f12b36dc6d9e3db9a2515af970e147ddb3568b3a
yamami mohamed monsif <mohammedyamami05@gmail.com>
2024-08-14 17:49:10 +0100

    Added functionality to the menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import logo from "../../../images/logo.svg";
 import NavBar from "./NavBar";
 import cartIcon from "../../../images/icon-cart.svg";
@@ -5,13 +6,29 @@ import avatar from "../../../images/image-avatar.png";
 import menuIcon from "../../../images/icon-menu.svg";
 
 function Header() {
+  const [visible, setVisible] = useState(false);
+  const [menuOpen, setMenuOpen] = useState("closed");
+  function handleClick() {
+    setVisible(!visible);
+    setMenuOpen((prev) => {
+      if (prev === "closed") {
+        return "open";
+      } else return "closed";
+    });
+  }
+
   return (
     <header>
-      <img src={menuIcon} alt="menu-icon" className="menu-icon" />
+      <button
+        className="menu-btn"
+        aria-controls="navigationBar"
+        data-menu={menuOpen}
+        onClick={handleClick}
+      ></button>
       <div className="logo-container">
         <img src={logo} alt="logo" />
       </div>
-      <NavBar />
+      <NavBar id="navigationBar" dataVisible={visible} />
       <div className="header-right">
         <div>
           <img src={cartIcon} alt="cart-icon" />
--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,24 +1,22 @@
-function NavBar() {
+function NavBar(props) {
   return (
-    <nav>
-      <ul className="navigation">
-        <li>
-          <a href="#">Collections</a>
-        </li>
-        <li>
-          <a href="#">Men</a>
-        </li>
-        <li>
-          <a href="#">Women</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Contacts</a>
-        </li>
-      </ul>
-    </nav>
+    <ul className="navigation" id={props.id} data-visible={props.dataVisible}>
+      <li>
+        <a href="#">Collections</a>
+      </li>
+      <li>
+        <a href="#">Men</a>
+      </li>
+      <li>
+        <a href="#">Women</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li>
+        <a href="#">Contacts</a>
+      </li>
+    </ul>
   );
 }
 
