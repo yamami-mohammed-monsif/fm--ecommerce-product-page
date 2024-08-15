61a12ceb3e213aceb4d560d4bf09f3a1cf8da6dd
yamami mohamed monsif <mohammedyamami05@gmail.com>
2024-08-15 13:00:15 +0100

    made the quantity control functional

--- src/components/main-component/QuantityControl.jsx
+++ src/components/main-component/QuantityControl.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
+
 function QuantityControl(props) {
+  const [quantity, setQuantity] = useState(0);
+  function handleQuantity(event) {
+    const number = parseInt(event.target.value);
+    setQuantity(number);
+  }
+
+  function increase() {
+    setQuantity((previous) => previous + 1);
+  }
+
+  function decrease() {
+    setQuantity((previous) => previous > 0 && previous - 1);
+  }
+
   return (
     <div className="quantity-control">
-      <img src={props.minus} alt="minus-icon" />
-      <input type="number" className="quantity-display" value={0} />
-      <img src={props.plus} alt="plus-icon" />
+      <img src={props.minus} alt="minus-icon" onClick={decrease} />
+      <input
+        type="number"
+        className="quantity-display"
+        onChange={handleQuantity}
+        value={quantity}
+      />
+      <img src={props.plus} alt="plus-icon" onClick={increase} />
     </div>
   );
 }
