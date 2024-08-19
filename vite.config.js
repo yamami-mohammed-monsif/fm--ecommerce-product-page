efa3a489cd8ef3ec0ec4eb79eab02ae9ad8e337e
yamami mohamed monsif <mohammedyamami05@gmail.com>
2024-08-19 14:47:55 +0100

    updated the base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ecommerce-product-page/",
+  base: "/fm--ecommerce-product-page/",
   plugins: [react()],
 });
